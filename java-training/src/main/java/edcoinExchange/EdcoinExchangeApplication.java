d1d76032dbb3ba12e5efd4afaf64954ed939371d
Eddie Pace <epace@scottlogic.com>
2021-09-14 15:50:05 +0100

    setup files in java-training dir

---
+++ java-training/src/main/java/edcoinExchange/EdcoinExchangeApplication.java
@@ -0,0 +1,13 @@
+package edcoinExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdcoinExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdcoinExchangeApplication.class, args);
+    }
+
+}
---
+++ java-training/src/test/java/edcoinExchange/EdcoinExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package edcoinExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdcoinExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
