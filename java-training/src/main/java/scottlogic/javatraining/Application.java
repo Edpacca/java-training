5581a4f37a11db3859d86f2842c2f952f56fca4e
Eddie Pace <epace@scottlogic.com>
2021-09-15 14:53:56 +0100

    rename main application file

--- java-training/src/main/java/scottlogic/javatraining/Application.java
+++ java-training/src/main/java/scottlogic/javatraining/Application.java
@@ -1,13 +1,13 @@
 package scottlogic.javatraining;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaTrainingApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaTrainingApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
+
 
 }
