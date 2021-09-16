105c07b16130e78d85d1c22cc77b06d5b8c9dcc6
Eddie Pace <epace@scottlogic.com>
2021-09-16 11:39:12 +0100

    implement lombok and add verification

--- java-training/src/main/java/scottlogic/javatraining/models/Order.java
+++ java-training/src/main/java/scottlogic/javatraining/models/Order.java
@@ -1,28 +1,28 @@
 package scottlogic.javatraining.models;
 
-import java.util.*;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
 public class Order {
     private final Date time = new Date();
     private final UUID id = UUID.randomUUID();
-    private UUID userId;
-    private Float quantity;
-    private Float price;
-    private Exchange exchange;
-    private Market market;
     private boolean isComplete = false;
 
-    public Order(UUID userId, float quantity, float price, Exchange exchange, Market market) {
-        this.userId = userId;
-        this.quantity = quantity;
-        this.price = price;
-        this.exchange = exchange;
-        this.market = market;
-    }
+    private final UUID userId;
+    private final Float price;
 
-    public float getQuantity() {
-        return quantity;
-    }
+    @NonNull @Setter
+    private Float quantity;
+
+    private final Exchange exchange;
+    private final Market market;
 
     public void reduceQuantity(float quantity) {
         if (this.quantity < quantity) {
@@ -32,33 +32,5 @@ public class Order {
         if (this.quantity == 0) {
             this.isComplete = true;
         }
-    }
-
-    public boolean isComplete() {
-        return this.isComplete;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public Market getMarket() {
-        return market;
-    }
-
-    public Exchange getExchange() {
-        return exchange;
-    }
-
-    public Date getTime() {
-        return time;
     }
 }
--- java-training/src/main/java/scottlogic/javatraining/models/Trade.java
+++ java-training/src/main/java/scottlogic/javatraining/models/Trade.java
@@ -1,65 +1,54 @@
 package scottlogic.javatraining.models;
 
+import lombok.Getter;
+
 import java.util.*;
 
-public class Trade {
+@Getter
+public final class Trade {
     private final Date time = new Date();
     private final UUID id = UUID.randomUUID();
-    private UUID buyOrderId;
-    private UUID buyUserId;
-    private UUID sellOrderId;
-    private UUID sellUserId;
-    private Float quantity;
-    private Float price;
-    private Market market;
+    private final UUID buyOrderId;
+    private final UUID buyUserId;
+    private final UUID sellOrderId;
+    private final UUID sellUserId;
+    private final Float quantity;
+    private final Float price;
+    private final Market market;
 
     public Trade(Order order1, Order order2) {
+        validateTrade(order1, order2);
         this.price = order1.getPrice();
         this.market = order1.getMarket();
         this.quantity = Math.min(order1.getQuantity(), order2.getQuantity());
-        parseOrders(order1, order2);
-        makeTrade(order1, order2);
-    }
 
-    private void makeTrade(Order order1, Order order2) {
-        order1.reduceQuantity(this.quantity);
-        order2.reduceQuantity(this.quantity);
-    }
-
-    private void parseOrders(Order order1, Order order2) {
         Order buyOrder = order1.getExchange() == Exchange.BUY ? order1 : order2;
         Order sellOrder = buyOrder == order1 ? order2 : order1;
         this.buyOrderId = buyOrder.getId();
         this.buyUserId = buyOrder.getUserId();
         this.sellOrderId = sellOrder.getId();
         this.sellUserId = sellOrder.getUserId();
+
+        makeTrade(order1, order2);
     }
 
-    public float getQuantity() {
-        return quantity;
+    private void validateTrade(Order order1, Order order2) {
+        if (order1.getId() == order2.getId()) {
+            throw new Error("Order cannot be traded with itself");
+        }
+        if (order1.getUserId() == order2.getUserId()) {
+            throw new Error("Orders created by same user cannot be traded");
+        }
+        if (order1.getExchange() == order2.getExchange()) {
+            throw new Error("Orders with same Exchange type cannot be traded");
+        }
+        if (order1.getMarket() != order2.getMarket()) {
+            throw new Error("Orders with different Market type cannot be traded");
+        }
     }
 
-    public float getPrice() {
-        return price;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public UUID[] getBuyIds() {
-        return new UUID[]{ buyOrderId, buyUserId };
-    }
-
-    public UUID[] getSellIds() {
-        return new UUID[]{ sellOrderId, sellUserId };
-    }
-
-    public Market getMarket() {
-        return market;
-    }
-
-    public Date getTime() {
-        return time;
+    private void makeTrade(Order order1, Order order2) {
+        order1.reduceQuantity(this.quantity);
+        order2.reduceQuantity(this.quantity);
     }
 }
