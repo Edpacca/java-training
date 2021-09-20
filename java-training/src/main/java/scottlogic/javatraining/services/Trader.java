4cd51c89e27bab26488bca08e3df57e59ebe7811
Eddie Pace <epace@scottlogic.com>
2021-09-20 11:09:39 +0100

    create and test Trader class

---
+++ java-training/src/main/java/scottlogic/javatraining/services/Trader.java
@@ -0,0 +1,38 @@
+package scottlogic.javatraining.services;
+
+import scottlogic.javatraining.models.Exchange;
+import scottlogic.javatraining.models.Order;
+import scottlogic.javatraining.models.Trade;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Trader {
+
+    public static List<Trade> makeTrades(List<Order> matchedOrders, Order newOrder) {
+
+        List<Trade> trades = new ArrayList<>();
+
+        if (newOrder.getExchange() == Exchange.SELL) {
+            matchedOrders.sort(Comparator
+                    .comparing(Order::getPrice)
+                    .thenComparing(Order::getTime));
+        } else {
+            matchedOrders.sort(Comparator
+                    .comparing(Order::getPrice)
+                    .reversed()
+                    .thenComparing(Order::getTime));
+        }
+
+        for (Order matchedOrder : matchedOrders) {
+            Trade trade = new Trade(matchedOrder, newOrder);
+            trades.add(trade);
+
+            if (newOrder.isComplete())
+                return trades;
+        }
+
+        return trades;
+    }
+}
