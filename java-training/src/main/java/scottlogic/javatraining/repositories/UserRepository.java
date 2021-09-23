679a0951796c17ee4568a6d6e231d07a3be6977f
Eddie Pace <epace@scottlogic.com>
2021-09-23 17:09:26 +0100

    remove unused methods and imports

--- java-training/src/main/java/scottlogic/javatraining/repositories/UserRepository.java
+++ java-training/src/main/java/scottlogic/javatraining/repositories/UserRepository.java
@@ -3,11 +3,9 @@ package scottlogic.javatraining.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import scottlogic.javatraining.models.UserAccount;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends MongoRepository<UserAccount, UUID> {
     Optional<UserAccount> findById(UUID id);
-    List<UserAccount> findByName(String name);
 }
