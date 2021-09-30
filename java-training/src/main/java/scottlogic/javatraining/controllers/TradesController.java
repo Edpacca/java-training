8b610351a6525dfe7cf1ab37d39d4b2e09e3bda4
Eddie Pace <epace@scottlogic.com>
2021-09-30 17:11:45 +0100

    Fix CORS

--- java-training/src/main/java/scottlogic/javatraining/controllers/TradesController.java
+++ java-training/src/main/java/scottlogic/javatraining/controllers/TradesController.java
@@ -2,10 +2,7 @@ package scottlogic.javatraining.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import scottlogic.javatraining.interfaces.ITradeService;
 import scottlogic.javatraining.models.Trade;
 
@@ -14,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("trades")
+@CrossOrigin
 public class TradesController {
 
     @Autowired
